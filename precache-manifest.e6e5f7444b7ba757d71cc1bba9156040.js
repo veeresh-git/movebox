cc1539f2709095db4013e816b3c83ba35b5adb0b
veeresh-git <60960189+veeresh-git@users.noreply.github.com>
2020-09-28 08:29:48 +0530

    Updates

---
+++ precache-manifest.e6e5f7444b7ba757d71cc1bba9156040.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99bd00258694bb2c2ebf2124d8e2d409",
+    "url": "/movebox/index.html"
+  },
+  {
+    "revision": "304c45a9d96a08940351",
+    "url": "/movebox/static/css/main.2d54fd69.chunk.css"
+  },
+  {
+    "revision": "c1e390ca92b338c9b5c4",
+    "url": "/movebox/static/js/2.f3f9d425.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movebox/static/js/2.f3f9d425.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "304c45a9d96a08940351",
+    "url": "/movebox/static/js/main.74206ec6.chunk.js"
+  },
+  {
+    "revision": "f1b159e1961cfce7e50b",
+    "url": "/movebox/static/js/runtime-main.ee76bfdf.js"
+  }
+]);
